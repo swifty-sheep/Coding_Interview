1b238076a7d8fc6850699976995237667d3cc1a1
swifty-sheep <tandavidtan32@gmail.com>
2021-02-14 21:04:43 -0600

    hash table? valid Sudoku

---
+++ leetcode_hash_table/36_valid_sudoku.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+
+    def is_row_valid(self, board):
+        for row in board:
+            if not self.is_unit_valid(row):
+                return False
+        return True
+
+    def is_col_valid(self, board):
+        for col in zip(*board):
+            if not self.is_unit_valid(col):
+                return False
+        return True
+
+    def is_square_valid(self, board):
+        for i in (0, 3, 6):
+            for j in (0, 3, 6):
+                unit = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if not self.is_unit_valid(unit):
+                    return False
+        return True
+
+    def is_unit_valid(self, unit):
+        unit = [i for i in unit if i != "."]
+        return len(set(unit)) == len(unit)
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        return self.is_row_valid(board) and self.is_col_valid(board) and self.is_square_valid(board)
+
