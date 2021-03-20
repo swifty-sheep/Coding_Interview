392a4eadc0f2b40895f97c98ead8e167c6290209
swifty-sheep <tandavidtan32@gmail.com>
2021-03-19 23:45:51 -0500

    Amazon OA | is robot bounded

---
+++ Amazon-OA/robot-bounded-in-circle.py
@@ -0,0 +1,55 @@
+"""
+1041. Robot Bounded In Circle
+https://leetcode.com/problems/robot-bounded-in-circle/
+"""
+
+
+class Solution:
+    @staticmethod
+    def is_robot_bounded(instructions: str) -> bool:
+        facing_direction = {
+            "n": [0, 1],
+            "e": [1, 0],
+            "s": [0, -1],
+            "w": [-1, 0],
+        }
+        next_left_direction = {
+            "n": "w",
+            "e": "n",
+            "s": "e",
+            "w": "s",
+        }
+        next_right_direction = {
+            "n": "e",
+            "e": "s",
+            "s": "w",
+            "w": "n",
+        }
+        current_facing = "n"
+        current_position = [0, 0]
+
+        for i in range(len(instructions)):
+            if instructions[i] == "G":
+                go = facing_direction[current_facing]
+                current_position[0] += go[0]
+                current_position[1] += go[1]
+            elif instructions[i] == "L":
+                current_facing = next_left_direction[current_facing]
+            elif instructions[i] == "R":
+                current_facing = next_right_direction[current_facing]
+
+        if current_facing == "n" and current_position != [0, 0]:
+            return False
+        return True
+
+
+def main():
+    instructions = "GGLLGG"
+    sol = Solution()
+    result = sol.is_robot_bounded(instructions=instructions)
+    print(result)
+
+
+# Driver Code
+if __name__ == "__main__":
+    main()
