469c5c701fc4016cdaa388497046672b787697e1
swifty-sheep <tandavidtan32@gmail.com>
2021-03-21 00:21:31 -0500

    Amazon OA | number of provinces - bfs

--- Amazon-OA/547-number-of-provinces.py
+++ Amazon-OA/547-number-of-provinces.py
@@ -29,10 +29,31 @@ def find_circle_num_dfs(a: List[List[int]]):
     return ans
 
 
+def find_circle_num_bfs(a: List[List[int]]):
+    seen = set()
+    res = 0
+    # for each city, if its not seen yet then create a queue and put it in there
+    for i in range(len(a)):
+        if i not in seen:
+            to_see = [i]
+            # while the queue is not empty, pop one city out and keep searching
+            # for the neighbor
+            while len(to_see):
+                current_city = to_see.pop()
+                if current_city not in seen:
+                    seen.add(current_city)
+                    to_see = [nei for nei, adj in enumerate(a[current_city]) if
+                              adj and nei not in seen] + to_see
+            res += 1
+    return res
+
+
 def main():
     is_connected = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
     ans = find_circle_num_dfs(is_connected)
     print(ans)
+    bfs_ans = find_circle_num_bfs(is_connected)
+    print(bfs_ans)
 
 
 # Driver Code
